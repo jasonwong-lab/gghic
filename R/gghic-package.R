5ec2542789cf93b6789aa35727790e46020694e7
Minghao Jiang | Office <jiangminghao1001@163.com>
2024-11-25 18:57:23 +0800

    Refactor import statements in gghic-package.R for consistency and clarity

--- R/gghic-package.R
+++ R/gghic-package.R
@@ -5,21 +5,21 @@
 #' @importFrom AnnotationDbi as.list loadDb saveDb
 #' @importFrom biovizBase getBioColor
 #' @importFrom dplyr bind_rows case_when distinct filter first group_by
-#'   left_join mutate row_number select slice slice_max slice_min summarise
-#'   ungroup
+#'   left_join mutate reframe rename row_number select slice slice_max
+#'   slice_min summarise ungroup
 #' @importFrom GenomicFeatures genes
-#' @importFrom GenomicRanges findOverlaps gaps GRanges GRangesList pintersect
-#'   reduce split
-#' @importFrom ggplot2 aes coord_fixed draw_key_blank draw_key_polygon
-#'   element_blank element_line Geom ggplot ggproto layer scale_fill_gradientn
-#'   scale_x_continuous Stat theme theme_bw %+replace%
+#' @importFrom GenomicRanges end findOverlaps gaps GRanges GRangesList
+#'   pintersect reduce split start
+#' @importFrom ggplot2 aes coord_fixed draw_key_blank draw_key_path
+#'   draw_key_polygon element_blank element_line Geom ggplot ggproto layer
+#' scale_fill_gradientn scale_x_continuous Stat theme theme_bw %+replace%
 #' @importFrom ggrastr rasterise
 #' @importFrom glue glue
 #' @importFrom grDevices colorRampPalette
-#' @importFrom grid arrow gList gpar nullGrob polygonGrob polylineGrob
-#'   segmentsGrob textGrob unit
+#' @importFrom grid arrow gList gpar is.unit nullGrob pathGrob pointsGrob
+#'   polygonGrob polylineGrob segmentsGrob textGrob unit
 #' @importFrom Gviz GeneRegionTrack
-#' @importFrom InteractionSet interactions pairdist
+#' @importFrom InteractionSet anchors interactions pairdist
 #' @importFrom IRanges IRanges overlapsAny
 #' @importFrom methods is
 #' @importFrom purrr map map2 map_df pmap
@@ -29,7 +29,7 @@
 #' @importFrom rtracklayer browserSession genome getTable import ucscTableQuery
 #' @importFrom S4Vectors mcols mcols<- subjectHits
 #' @importFrom scales oob_squish unit_format
-#' @importFrom stats end setNames start
+#' @importFrom stats setNames
 #' @importFrom tibble as_tibble is_tibble tibble
 #' @importFrom tidyr drop_na replace_na
 #' @importFrom txdbmaker makeTxDb makeTxDbFromGFF
