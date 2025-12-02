81e27757c534adc13a2deab7e4b3fed32662a715
Minghao Jiang | Office <jiangminghao1001@163.com>
2025-12-02 17:44:41 +0800

    Refactor geom_loop and geom_loop2 functions to improve parameter handling and streamline loop drawing styles

--- R/geom-loop.R
+++ R/geom-loop.R
@@ -4,15 +4,9 @@ StatLoop <- ggplot2::ggproto(
   required_aes = c(
     "seqnames1", "start1", "end1", "seqnames2", "start2", "end2"
   ),
-  extra_params = c(
-    ggplot2::Stat$extra_params
-  ),
-  dropped_aes = c(
-    "seqnames1", "start1", "end1", "seqnames2", "start2", "end2", "fill"
-  ),
+  setup_params = function(data, params) params,
   compute_panel = function(
-    data, scales,
-    loop_path, loop_gis, is_0based
+    data, scales, loop_path = NULL, loop_gis = NULL, is_0_based = FALSE
   ) {
     # ======================================================================== #
     #   ^        /\                                                            #
@@ -26,26 +20,31 @@ StatLoop <- ggplot2::ggproto(
     # --+--------------------------------------------------------------------> #
     #   |                                                                      #
     # ======================================================================== #
-    name_pkg <- get_pkg_name()
+    name_pkg <- .getPkgName()
     env <- get(".env", envir = asNamespace(name_pkg))
     if (env$n_hic == 1) {
       res <- env$res
       n_sn <- env$n_sn
+      MIN_Y <- env$MIN_Y
     } else {
+      dat_hic <- data |>
+        .calculateHicCoordinates()
       res <- data$end1[1] - data$start1[1] + 1
       n_sn <- length(unique(c(data$seqnames1, data$seqnames2)))
       env$res <- res
       env$n_sn <- n_sn
+      env$MIN_Y <- min(dat_hic$y, na.rm = TRUE)
+      MIN_Y <- env$MIN_Y
     }
 
     if (!is.null(loop_path)) {
-      tmp <- loop_path |>
-        vroom::vroom(
-          delim = "\t", col_types = vroom::cols(),
-          col_names = c("chrom1", "start1", "end1", "chrom2", "start2", "end2")
+      tmp <- read.delim(
+        loop_path, header = FALSE, col.names = c(
+          "chrom1", "start1", "end1", "chrom2", "start2", "end2"
         )
+      )
 
-      if (is_0based) {
+      if (is_0_based) {
         tmp$start1 <- tmp$start1 + 1
         tmp$start2 <- tmp$start2 + 1
       }
@@ -60,20 +59,21 @@ StatLoop <- ggplot2::ggproto(
       )
       gis_loop <- InteractionSet::GInteractions(anchor1, anchor2)
     }
+
     if (!is.null(loop_gis)) gis_loop <- loop_gis
 
     if (is.null(env$gis)) {
-      gis_data <- tbl2gis(data)
+      gis_data <- .tbl2Gis(data)
     } else {
       gis_data <- env$gis
     }
 
     .first <- InteractionSet::anchors(gis_loop)$first
     .second <- InteractionSet::anchors(gis_loop)$second
-    seqlevels(.first) <- seqlevels(gis_data)
-    seqlevels(.second) <- seqlevels(gis_data)
-    GenomeInfoDb::seqinfo(.first) <- GenomeInfoDb::seqinfo(gis_data)
-    GenomeInfoDb::seqinfo(.second) <- GenomeInfoDb::seqinfo(gis_data)
+    Seqinfo::seqlevels(.first) <- Seqinfo::seqlevels(gis_data)
+    Seqinfo::seqlevels(.second) <- Seqinfo::seqlevels(gis_data)
+    Seqinfo::seqinfo(.first) <- Seqinfo::seqinfo(gis_data)
+    Seqinfo::seqinfo(.second) <- Seqinfo::seqinfo(gis_data)
 
     to_keep1 <- IRanges::overlapsAny(.first, gis_data)
     to_keep2 <- IRanges::overlapsAny(.second, gis_data)
@@ -84,17 +84,17 @@ StatLoop <- ggplot2::ggproto(
 
     if ((n_sn > 1 || (n_sn == 2 && any(data$seqnames1 == data$seqnames2)))) {
       chroms_add <- data |>
-        calculate_add_lengths()
+        .calculateAddLengths()
       chroms_sub <- data |>
-        calculate_subtract_lengths()
+        .calculateSubtractLengths()
 
       dat_loop <- dat_loop |>
         dplyr::rename(seqname = seqnames1) |>
-        adjust_coordinates2(
+        .adjustCoordinates2(
           chroms_add, chroms_sub, c(start1 = "start1", end1 = "end1")
         ) |>
         dplyr::rename(seqname1 = seqname, seqname = seqnames2) |>
-        adjust_coordinates2(
+        .adjustCoordinates2(
           chroms_add, chroms_sub, c(start2 = "start2", end2 = "end2")
         ) |>
         dplyr::rename(seqname2 = seqname)
@@ -103,43 +103,67 @@ StatLoop <- ggplot2::ggproto(
     dat <- dat_loop |>
       dplyr::mutate(
         x = (start1 + end2) / 2,
-        y = (x - start1)
+        y = (x - start1),
+        xmin = start1,
+        xmax = end2,
+        ymin = MIN_Y
       )
 
     dat
   }
 )
 
-GeomLoop <- ggproto(
+GeomLoop <- ggplot2::ggproto(
   "GeomLoop",
   ggplot2::GeomPoint,
   required_aes = c("x", "y"),
-  extra_params = c(
-    ggplot2::Geom$extra_params,
-    "colour", "shape", "fill", "size", "linewidth"
-  ),
   draw_key = ggplot2::draw_key_point,
-  default_aes = NULL,
+  default_aes = ggplot2::aes(
+    colour = "black", shape = 21, fill = NA,
+    size = grid::unit(1 / 80, "native"), stroke = 1
+  ),
   draw_panel = function(
-    data, panel_params, coord,
-    colour, shape, fill, size, linewidth
+    data, panel_params, coord, colour = "black", shape = 21, fill = NA,
+    size = NULL, stroke = 1, style = "circle", n_arc_points = 50
   ) {
     coords <- coord$transform(data, panel_params)
 
-    if (is.null(size)) {
-      size <- grid::unit(1 / 80, "native")
-    } else if (is.numeric(size)) {
-      size <- grid::unit(size, "mm")
-    } else if (grid::is.unit(size)) {
-      size <- size
-    }
+    if (style == "arc") {
+      grobs <- lapply(seq_len(nrow(coords)), function(i) {
+        xmin <- coords$xmin[i]
+        xmax <- coords$xmax[i]
+        width <- xmax - xmin
+        height <- coords$y[i] - coords$ymin[i]
+        t <- seq(0, pi, length.out = n_arc_points)
+        x_points <- xmin + width / 2 + (width / 2) * cos(t)
+        y_points <- coords$ymin[i] + height * sin(t)
 
-    grid::pointsGrob(
-      coords$x, coords$y,
-      pch = shape, size = size,
-      default.units = "native",
-      gp = grid::gpar(col = colour, fill = fill, lwd = linewidth)
-    )
+        grid::polylineGrob(
+          x = x_points, y = y_points,
+          default.units = "native",
+          gp = grid::gpar(
+            col = coords$colour[i] %||% colour,
+            lwd = coords$stroke[i] %||% stroke
+          )
+        )
+      })
+      do.call(grid::gList, grobs)
+    } else {
+      if (is.null(size)) {
+        size <- grid::unit(1 / 80, "native")
+      } else if (is.numeric(size)) {
+        size <- grid::unit(size, "mm")
+      } else if (grid::is.unit(size)) {
+        size <- size
+      }
+
+      grid::pointsGrob(
+        coords$x, coords$y,
+        pch = shape, size = size,
+        default.units = "native",
+        gp = grid::gpar(col = colour, fill = fill, lwd = stroke)
+      )
+    }
   }
 )
 
@@ -150,13 +174,21 @@ GeomLoop <- ggproto(
 #' @inheritParams geom_hic
 #' @param loop_path A path to the loop file. Default is `NULL`.
 #' @param loop_gis An InteractionSet object of loops. Default is `NULL`.
-#' @param is_0based Whether the loop file is 0-based or not. Default is `FALSE`.
-#' @param colour The color of the loops. Default is `grey`.
-#' @param shape The shape of the loops. Default is `21`.
-#' @param fill The fill color of the loops. Default is `NA`.
-#' @param size The size of the loops. Default is `NULL`.
-#'   If `NULL`, the size is `grid::unit(1 / 80, "native")`.
-#' @param linewidth The line width of the loops. Default is `1`.
+#' @param is_0_based Whether the loop file is 0-based or not.
+#'   Default is `FALSE`.
+#' @param style The style for drawing loops: `"circle"` for points/circles
+#'   or `"arc"` for arcs under the Hi-C heatmap. Default is `"circle"`.
+#' @param n_arc_points Number of points used to draw each arc (only used when
+#'   `style = "arc"`). Default is `50`.
+#' @param colour The color of the loops. Default is `"black"`.
+#' @param shape The shape of the loops (only used when `style = "circle"`).
+#'   Default is `21`.
+#' @param fill The fill color of the loops
+#'   (only used when `style = "circle"`).
+#'   Default is `NA`.
+#' @param size The size of the loops (only used when `style = "circle"`).
+#'   Default is `grid::unit(1 / 80, "native")`.
+#' @param stroke The line width of the loops. Default is `1`.
 #' @param ... Parameters to be ignored.
 #' @details
 #' Requires the following aesthetics:
@@ -169,68 +201,35 @@ GeomLoop <- ggproto(
 #' @return A ggplot object.
 #' @examples
 #' \dontrun{
-#' library(gghic)
-#' library(ggplot2)
-#' library(dplyr)
-#' library(HiCExperiment)
-#' library(InteractionSet)
-#' library(scales)
-#' library(glue)
-#' library(rappdirs)
-#'
-#' dir_cache_gghic <- user_cache_dir(appname = "gghic")
-#' url_file <- paste0(
-#'   "https://raw.githubusercontent.com/mhjiang97/gghic-data/refs/heads/",
-#'   "master/cooler/chr4_11-100kb.cool"
-#' )
-#' path_file <- file.path(dir_cache_gghic, "chr4_11-100kb.cool")
-#' download.file(url_file, path_file)
-#'
-#' hic <- path_file |>
-#'   CoolFile() |>
+#' # Load Hi-C data
+#' cc <- ChromatinContacts("path/to/cooler.cool", focus = "chr4") |>
 #'   import()
 #'
-#' gis <- interactions(hic)
-#' gis$score <- log10(gis$balanced)
-#' x <- as_tibble(gis)
-#' scores <- x$score[pairdist(gis) != 0 & !is.na(pairdist(gis) != 0)]
-#' scores <- scores[!is.na(scores) & !is.infinite(scores)]
-#' x$score <- oob_squish(x$score, c(min(scores), max(scores)))
+#' # Add loops from file
+#' loop_file <- "path/to/loops.bedpe"
+#' gghic(cc) + geom_loop(loop_path = loop_file)
 #'
-#' p <- x |>
-#'   filter(
-#'     seqnames1 == "chr4", seqnames2 == "chr4",
-#'     center1 > 11000000 & center1 < 21000000,
-#'     center2 > 11000000 & center2 < 21000000
-#'   ) |>
-#'   gghic(expand_xaxis = TRUE)
-#'
-#' url_file <- paste0(
-#'   "https://raw.githubusercontent.com/mhjiang97/gghic-data/refs/heads/",
-#'   "master/loop/loops-chr4_11.txt"
-#' )
-#' path_loop <- glue("{dir_cache_gghic}/loops-chr4_11.txt")
-#' download.file(url_file, path_loop)
-#'
-#' p + geom_loop(loop_path = path_loop, is_0based = TRUE)
+#' # Draw loops as arcs
+#' gghic(cc) + geom_loop(loop_path = loop_file, style = "arc")
 #' }
-#' @export geom_loop
+#' @export
 #' @aliases geom_loop
 geom_loop <- function(
   mapping = NULL, data = NULL, stat = StatLoop, position = "identity",
-  na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...,
-  loop_path = NULL, loop_gis = NULL, is_0based = FALSE,
-  colour = "black", shape = 21, fill = NA, size = NULL, linewidth = 1
+  na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, loop_path = NULL,
+  loop_gis = NULL, is_0_based = FALSE, style = "circle",
+  n_arc_points = 50, colour = "black", shape = 21, fill = NA,
+  size = grid::unit(1 / 80, "native"), stroke = 1, ...
 ) {
   ggplot2::layer(
     geom = GeomLoop, mapping = mapping, data = data, stat = stat,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     check.param = FALSE,
     params = list(
-      na.rm = na.rm, ...,
-      loop_path = loop_path, loop_gis = loop_gis, is_0based = is_0based,
-      colour = colour, shape = shape, fill = fill, size = size,
-      linewidth = linewidth
+      na.rm = na.rm, loop_path = loop_path, loop_gis = loop_gis,
+      is_0_based = is_0_based, style = style,
+      n_arc_points = n_arc_points, colour = colour, shape = shape, fill = fill,
+      size = size, stroke = stroke, ...
     )
   )
 }
--- R/geom-loop2.R
+++ R/geom-loop2.R
@@ -1,17 +1,22 @@
+#' @rdname geom_loop2
+#' @format NULL
+#' @usage NULL
+#' @export
 StatLoop2 <- ggplot2::ggproto(
   "StatLoop2",
   ggplot2::Stat,
   required_aes = c(
     "seqnames1", "start1", "end1", "seqnames2", "start2", "end2"
   ),
-  extra_params = c(ggplot2::Stat$extra_params),
+  setup_params = function(data, params) params,
   compute_panel = function(data, scales) {
-    name_pkg <- get_pkg_name()
+    name_pkg <- .getPkgName()
     env <- get(".env", envir = asNamespace(name_pkg))
     if (env$n_hic > 0) {
       n_sn <- env$n_sn
       chroms_add <- env$chroms_add
       chroms_sub <- env$chroms_sub
+      MIN_Y <- env$MIN_Y
     } else {
       stop("geom_loop2() requires a HiC plot to be drawn first.")
     }
@@ -19,11 +24,11 @@ StatLoop2 <- ggplot2::ggproto(
     if (!is.null(chroms_add) && !is.null(chroms_sub)) {
       data <- data |>
         dplyr::rename(seqname = seqnames1) |>
-        adjust_coordinates2(
+        .adjustCoordinates2(
           chroms_add, chroms_sub, c(start1 = "start1", end1 = "end1")
         ) |>
         dplyr::rename(seqname1 = seqname, seqname = seqnames2) |>
-        adjust_coordinates2(
+        .adjustCoordinates2(
           chroms_add, chroms_sub, c(start2 = "start2", end2 = "end2")
         ) |>
         dplyr::rename(seqname2 = seqname)
@@ -32,42 +37,72 @@ StatLoop2 <- ggplot2::ggproto(
     dat <- data |>
       dplyr::mutate(
         x = (start1 + end2) / 2,
-        y = (x - start1)
+        y = (x - start1),
+        xmin = start1,
+        xmax = end2,
+        ymin = MIN_Y
       )
 
     dat
   }
 )
 
-GeomLoop2 <- ggproto(
+#' @rdname geom_loop2
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomLoop2 <- ggplot2::ggproto(
   "GeomLoop2",
   ggplot2::Geom,
   required_aes = c("x", "y"),
-  extra_params = c(ggplot2::Geom$extra_params),
   draw_key = ggplot2::draw_key_point,
   default_aes = ggplot2::aes(
-    colour = "black", shape = 21, fill = NA, size = 1, fontsize = 3, alpha = 1,
-    stroke = 0.5
+    colour = "black", shape = 21, fill = NA, size = 1, fontsize = 3,
+    alpha = 1, stroke = 0.5
   ),
-  draw_panel = function(data, panel_params, coord) {
+  draw_panel = function(
+    data, panel_params, coord, style = "circle", n_arc_points = 50
+  ) {
     coords <- coord$transform(data, panel_params)
 
-    if (is.null(data$size)) {
-      size <- grid::unit(1 / 80, "native")
-    } else if (is.numeric(data$size)) {
-      size <- grid::unit(data$size, "mm")
-    } else if (grid::is.unit(data$size)) {
-      size <- data$size
-    }
+    if (style == "arc") {
+      grobs <- lapply(seq_len(nrow(coords)), function(i) {
+        xmin <- coords$xmin[i]
+        xmax <- coords$xmax[i]
+        width <- xmax - xmin
+        height <- coords$y[i] - coords$ymin[i]
+        t <- seq(0, pi, length.out = n_arc_points)
+        x_points <- xmin + width / 2 + (width / 2) * cos(t)
+        y_points <- coords$ymin[i] + height * sin(t)
 
-    grid::pointsGrob(
-      coords$x, coords$y,
-      pch = coords$shape, size = size,
-      default.units = "native",
-      gp = grid::gpar(
-        col = coords$colour, fill = coords$fill, lwd = coords$stroke
+        grid::polylineGrob(
+          x = x_points, y = y_points,
+          default.units = "native",
+          gp = grid::gpar(
+            col = coords$colour[i],
+            lwd = coords$stroke[i],
+            alpha = coords$alpha[i]
+          )
+        )
+      })
+      do.call(grid::gList, grobs)
+    } else {
+      if (is.null(data$size)) {
+        size <- grid::unit(1 / 80, "native")
+      } else if (is.numeric(data$size)) {
+        size <- grid::unit(data$size, "mm")
+      } else if (grid::is.unit(data$size)) {
+        size <- data$size
+      }
+
+      grid::pointsGrob(
+        coords$x, coords$y,
+        pch = coords$shape, size = size, default.units = "native",
+        gp = grid::gpar(
+          col = coords$colour, fill = coords$fill, lwd = coords$stroke
+        )
       )
-    )
+    }
   }
 )
 
@@ -76,6 +111,10 @@ GeomLoop2 <- ggproto(
 #' @description A second version of [geom_loop()].
 #' @inheritParams ggplot2::geom_point
 #' @inheritParams geom_hic
+#' @param style The style for drawing loops: `"circle"` for points/circles
+#'   or `"arc"` for arcs under the Hi-C heatmap. Default is `"circle"`.
+#' @param n_arc_points Number of points used to draw each arc (only used when
+#'   `style = "arc"`). Default is `50`.
 #' @param ... Parameters to be ignored.
 #' @details
 #' Requires the following aesthetics:
@@ -88,17 +127,48 @@ GeomLoop2 <- ggproto(
 #' @return A ggplot object.
 #' @examples
 #' \dontrun{
-#' # example usage
+#' # Load Hi-C data
+#' cc <- ChromatinContacts("path/to/cooler.cool", focus = "chr4") |>
+#'   import()
+#'
+#' # Prepare loop data frame
+#' loop_file <- "path/to/loops.bedpe"
+#' loops <- read.table(
+#'   loop_file, col.names = c(
+#'     "chr1", "start1", "end1", "chr2", "start2", "end2"
+#'   )
+#' )
+#'
+#' # Draw loops as circles (default)
+#' gghic(cc) +
+#'   geom_loop2(
+#'     data = loops, aes(
+#'       seqnames1 = chr1, start1 = start1, end1 = end1,
+#'       seqnames2 = chr2, start2 = start2, end2 = end2
+#'     )
+#'   )
+#'
+#' # Draw loops as arcs
+#' gghic(cc) +
+#'   geom_loop2(
+#'     data = loops, aes(
+#'       seqnames1 = chr1, start1 = start1, end1 = end1,
+#'       seqnames2 = chr2, start2 = start2, end2 = end2
+#'     ), style = "arc"
+#'   )
 #' }
-#' @export geom_loop2
+#' @export
 #' @aliases geom_loop2
 geom_loop2 <- function(
   mapping = NULL, data = NULL, stat = StatLoop2, position = "identity",
-  na.rm = FALSE, show.legend = NA, inherit.aes = FALSE, ...
+  na.rm = FALSE, show.legend = NA, inherit.aes = FALSE,
+  style = "circle", n_arc_points = 50, ...
 ) {
   ggplot2::layer(
     geom = GeomLoop2, mapping = mapping, data = data, stat = stat,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    check.param = TRUE, params = list(na.rm = na.rm, ...)
+    check.param = TRUE,
+    params = list(na.rm = na.rm, style = style,
+                  n_arc_points = n_arc_points, ...)
   )
 }
